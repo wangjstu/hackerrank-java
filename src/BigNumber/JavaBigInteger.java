542b7560f9f8f36c2462cc1d68e5abd19c7e813a
wangjstu <wangjstu@gmail.com>
2023-03-30 12:31:44 +0800

    java-BigInteger 大数字（200位）加法、乘法

--- src/BigNumber/JavaBigInteger.java
+++ src/BigNumber/JavaBigInteger.java
@@ -1,4 +1,18 @@
 package src.BigNumber;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/java-biginteger/problem?isFullScreen=true
+ */
 public class JavaBigInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BigInteger bigIntegerA = scanner.nextBigInteger();
+        BigInteger bigIntegerB = scanner.nextBigInteger();
+        System.out.println(bigIntegerA.add(bigIntegerB));
+        System.out.println(bigIntegerA.multiply(bigIntegerB));
+        scanner.close();
+    }
 }
