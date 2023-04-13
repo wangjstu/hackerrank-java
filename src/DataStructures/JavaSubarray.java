ec06ebba4e933a117086b5a89104e28a34b55a81
wangjstu <wangjstu@gmail.com>
2023-04-13 18:46:12 +0800

    java-data structures-array-一维数组操作subarr没有用

---
+++ src/DataStructures/JavaSubarray.java
@@ -0,0 +1,38 @@
+package src.DataStructures;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * https://www.hackerrank.com/challenges/java-negative-subarray/problem?isFullScreen=true
+ * Sample Input
+ *
+ * 5
+ * 1 -2 4 -5 1
+ *
+ * Sample Output
+ * 9
+ *
+ */
+public class JavaSubarray {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        Integer n = Integer.parseInt(bufferedReader.readLine());
+        Integer[] arr = Arrays.stream(bufferedReader.readLine().replaceAll("\\s+$", "").split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+        Integer count = 0;
+        for (Integer i = 0; i < n; i++) {
+            Integer sum = 0;
+            for (int j = i; j < n; j++) {
+                sum+=arr[j];
+                if (sum.compareTo(0)<0) {
+                     count++;
+                }
+            }
+        }
+        System.out.println(count);
+        bufferedReader.close();
+    }
+}
